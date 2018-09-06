4968caf686468966ebe5d37a4d0bb1758f5e67c5
Alejandro Vivas <alejandrojv17@gmail.com>
2018-09-06 13:40:33 -0300

    Añadido CORS

--- GenbetaDevWebApi/App_Start/WebApiConfig.cs
+++ GenbetaDevWebApi/App_Start/WebApiConfig.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace GenbetaDevWebApi
 {
@@ -10,6 +11,10 @@ namespace GenbetaDevWebApi
         public static void Register(HttpConfiguration config)
         {
             // Configuración y servicios de API web
+
+            //Habilitar CORS
+            var urlPermitidas = new EnableCorsAttribute("*", "*", "*");
+            config.EnableCors(urlPermitidas);
 
             // Rutas de API web
             config.MapHttpAttributeRoutes();
